c3096feecea742127393128561474c25ed431918
Shravya-27 <96378781+Shravya-27@users.noreply.github.com>
2023-01-08 17:46:54 +0530

    program1(consonant or not).java

---
+++ program1(consonant or not).java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class Main
+{
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+String str;
+
+System.out.println("Enter the string");
+   str=sc.nextLine();
+for(int i=0;i<str.length();i++)
+   {
+if(str.charAt(i) !='a'&& str.charAt(i) !='e' && str.charAt(i) !='i' && str.charAt(i) !='o' && str.charAt(i) !='u')
+           System.out.println("Given string contains consonant"+" "+str.charAt(i)+" "+"in the index"+" "+i);
+   }
+}
+}
+
+
